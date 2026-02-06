38545daf714eb8bc981028de53c17dee7f1c8a4b
iamDaedalus <jerometecson.basc@gmail.com>
2026-02-06 14:45:52 +0800

    3434

--- app/player/[...params]/hooks/useServerManager.ts
+++ app/player/[...params]/hooks/useServerManager.ts
@@ -38,6 +38,7 @@ export function useServerManager({
   defaultServer: number;
 }) {
   const [servers, setServers] = useState<ServerTypes[]>([
+    { name: "Icarus", server: 11, status: "queue", desc: "New Server" },
     { name: "Daedalus I", server: 0, status: "queue", desc: "Main Server" },
 
     {
